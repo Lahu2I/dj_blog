c87129c4b0218b425fca0558a9bfc55ced0425ea
Lahu2I <lahuchavan2@gmail.com>
2020-08-09 23:08:21 +0530

    changes in save method

--- users/apps.py
+++ users/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     name = 'users'
 
-    def ready(self):
+    def ready(self):  # method just to import the signals
+
         import users.signals
--- users/models.py
+++ users/models.py
@@ -12,7 +12,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, args, *kwargs):
+    def save(self, *args, **kwargs):
+
         super().save(*args, **kwargs)  # calling parent class save method
 
         img = Image.open(self.image.path)
